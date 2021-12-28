3130a8c8c3c2a03e58ad736e5f41d397d6d182dd
HuritaLal <69113689+HuritaLal@users.noreply.github.com>
2021-12-28 15:53:04 +0530

    Add files via upload

---
+++ components/Game.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent("game-play",{
+    schema:{
+        element:{
+            type:'string',
+            default:"#ring1"
+        },
+    },
+    update:function(){
+        this.isCollided(this.data.elementId)
+    },
+    isCollided:function(elementId){
+        const element=document.querySelector(elementId)
+        element.addEventListener("collide",(e)=>{
+            if(elementId.includes("#ring1")){
+                console.log(elementId+"collision")
+            }
+            else if(elementId.includes("#hurdle")){
+                console.log("birdCollision")
+            }
+        })
+    }
+})
